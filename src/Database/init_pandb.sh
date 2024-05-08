5b51f569a0221c673e46da4e552d3b86fac5d137
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-05-08 16:11:53 +0800

    主节点数据库更名为pan_masterdb

--- src/Database/init_pandb.sh
+++ src/Database/init_pandb.sh
@@ -3,7 +3,9 @@
 # 初始化数据库
 
 echo "创建用户数据库"
-
-createdb pan_userdb
-psql -d pan_userdb -f uuid.sql
-psql -d pan_userdb -f user.sql
+MASTERDB=pan_master_db
+dropdb $MASTERDB
+createdb $MASTERDB
+psql -d $MASTERDB -f miscellaneous.sql
+psql -d $MASTERDB -f uuid.sql
+psql -d $MASTERDB -f user.sql
