c32cd08cfaa53025799f0dd1ef67518608be0f6b
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-05-11 12:23:44 +0800

    Update UserServer.php

--- src/MasterService/UserServer.php
+++ src/MasterService/UserServer.php
@@ -2,11 +2,19 @@
 
 include './DBConf.php';
 include './Server.php';
+
 /**
  * Description of UserServer
  *
  * @author pcwang
  */
 class UserServer extends Server {
-    //put your code here
+
+    #[\Override]
+    protected function process() {
+        $this->_response = array(
+            "success" => false,
+            "message" => "用户服务"
+        );
+    }
 }
