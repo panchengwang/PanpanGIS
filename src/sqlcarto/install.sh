bfe8da1f6012c1c29e6fee84626a72be3243deb6
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-05-11 17:33:11 +0800

    Update install.sh

--- src/sqlcarto/install.sh
+++ src/sqlcarto/install.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 VERSION=1.0
-SQL_FILE=sqlcarto--${VERSION}.sql
-cat ./sqlcarto.sql > $SQL_FILE
-cat ./email.sql >> $SQL_FILE
+
 
 PG_SRC_PATH=/d/software/sdb/postgresql-16.0
-
+rm -rf $PG_SRC_PATH/contrib/sqlcarto
 mkdir $PG_SRC_PATH/contrib/sqlcarto
 cp -rf ../sqlcarto/*  $PG_SRC_PATH/contrib/sqlcarto
 cd $PG_SRC_PATH/contrib/sqlcarto
 make
+
+SQL_FILE=sqlcarto--${VERSION}.sql
+cat ./sqlcarto.sql > $SQL_FILE
+cat ./email.sql >> $SQL_FILE
+
 make install
 ./test.sh
