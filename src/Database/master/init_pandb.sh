120cc35f6b097b849ccf4f511ab373b1e08d4cef
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-05-13 11:41:45 +0800

    位置移动

---
+++ src/Database/master/init_pandb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 初始化数据库
+CUR_DIR=$(cd $(dirname $0); pwd)
+
+#创建控制节点数据库
+MASTERDB=pan_master_db
+dropdb $MASTERDB
+createdb $MASTERDB
+cat $CUR_DIR/miscellaneous.sql > pan_master_db.sql
+cat $CUR_DIR/user.sql >>  pan_master_db.sql
+
+psql -d $MASTERDB -f pan_master_db.sql
+
