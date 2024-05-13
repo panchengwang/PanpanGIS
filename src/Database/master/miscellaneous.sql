4d8161f0fbb289b19a801212aee56682c74e1acb
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-05-13 11:42:09 +0800

    panpangis数据库最低要求

---
+++ src/Database/master/miscellaneous.sql
@@ -0,0 +1,12 @@
+-- 系统需要的一些杂项函数
+create extension "uuid-ossp";
+create extension "sqlcarto";
+
+set client_encoding to utf8;
+
+create or replace function pan_requirement() returns text as 
+$$
+    select 'panpangis 数据库端需要
+            数据库扩展：ossp-uuid, sqlcarto';
+$$ language 'sql';
+
