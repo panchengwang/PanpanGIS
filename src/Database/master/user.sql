6f6b2de54e2885231f5af9cc86f672e72cf16e94
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-05-13 15:58:05 +0800

    Update user.sql

--- src/Database/master/user.sql
+++ src/Database/master/user.sql
@@ -1,6 +1,9 @@
 -- 用户数据库初始化
 
-
+-- 邮件发送参数配置
+update sc_configuration set key_value = 'sqlcartotest@126.com' where key_name = 'EMAIL_USER';
+update sc_configuration set key_value = 'smtps://smtp.126.com:465' where key_name = 'EMAIL_SMTP';
+update sc_configuration set key_value = 'SCUGOXHGWAEZUEQH' where key_name = 'EMAIL_PASSWORD';
 
 -- 用户数据库
 drop table if exists pan_user;
@@ -8,11 +11,12 @@ create table pan_user(
     id varchar(32) default sc_uuid() primary key,
     username varchar(64) unique not null,
     nickname varchar(64) NOT NULL default '',
-    password varchar(128) not null,
+    password varchar(128) not null default '',
     identify_code varchar(8) default '',
     identify_code_expire timestamp default now() + interval '30 minutes',
     token varchar(128) not null  default '',
-    token_expire timestamp default now() + interval '5 minutes'
+    token_expire timestamp default now() + interval '5 minutes',
+    status integer      -- 1 有效用户，2 失效用户，3 注册状态
 );
 
 -- 系统管理员账号
@@ -27,3 +31,9 @@ insert into pan_user(username,password)
 values
     ('pcwang251@163.com', md5('pcwang251'));
 
+
+-- 用户是否存在
+create or replace function pan_user_exist(username varchar) returns boolean as 
+$$
+    select count(1)=1 from pan_user where username = $1;
+$$ language 'sql';
