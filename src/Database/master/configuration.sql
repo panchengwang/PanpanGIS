93a70ef989beb6d311793fe51dbe1cebf3e320e0
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-06-04 17:49:57 +0800

    panpangis的后台参数配置

---
+++ src/Database/master/configuration.sql
@@ -0,0 +1,19 @@
+-- 系统配置参数
+
+create table pan_configuration(
+    keyname varchar unique not null,
+    keyvalue varchar 
+) ;
+
+insert into pan_configuration(keyname,keyvalue) 
+values 
+    ('IDENTIFY_CODE_VALID_TIME', '10 minutes')          -- 验证码有效时长
+    ;
+
+-- 获取配置
+create or replace function pan_get_configuration(
+    keyname varchar
+) returns varchar as 
+$$
+    select keyvalue from pan_configuration where keyname = $1;
+$$ language 'sql';
