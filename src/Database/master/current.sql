123e73432b0ebb9bddacf7f989086310651d4c8a
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-06-05 18:03:17 +0800

    Update current.sql

--- src/Database/master/current.sql
+++ src/Database/master/current.sql
@@ -6,4 +6,37 @@ set client_encoding to utf8;
 --      用户相关的sql拷贝到 user.sql
 --      服务相关的sql拷贝到 server.sql
 
+create or replace function pan_add_server(
+    dbhost varchar,
+    dbport integer,
+    dbname varchar,
+    dbuser varchar,
+    dbpassword varchar,
+    weburl varchar
+) returns integer as 
+$$
+declare
+    maxid integer;
+begin
+    select max(id) into maxid from pan_server;
+    if maxid is null then
+        maxid := 1;
+    else 
+        maxid := maxid + 1 ;
+    end if;
 
+    insert into pan_server(id,db_host,db_port,db_name,db_user,db_password, web_url) 
+    values
+        (maxid, dbhost,dbport,dbname,dbuser,dbpassword,weburl);
+    return maxid;
+end;
+$$ language 'plpgsql';
+
+select pan_add_server(
+    '127.0.0.1',
+    5432,
+    'pan_node_db',
+    'pcwang',
+    '',
+    'https://127.0.0.1/'
+);
